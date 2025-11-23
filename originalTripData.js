a455e5567b2c304c48a04062be5a0f5d3d92ca2c
HaoKuan0201 <72717345+HaoKuan0201@users.noreply.github.com>
2025-11-23 21:12:20 +0800

    Create originalTripData.js

---
+++ originalTripData.js
@@ -0,0 +1,455 @@
+const originalTripData = {
+    "title": "曼谷五日自由行 (11/27 - 12/1)",
+    "days": [
+        {
+            "day": "Day 1",
+            "date": "11/27 (四)",
+            "fullDate": "2025-11-27",
+            "theme": "抵達與夜市",
+            "events": [
+                {
+                    "time": "13:40-16:00",
+                    "location": "Thai Lion Air SL391",
+                    "mapURL": "",
+                    "transport": "飛機",
+                    "cost": "0",
+                    "notes": ""
+                },
+                {
+                    "time": "16:05",
+                    "location": "抵達 DMK 機場",
+                    "transport": "無",
+                    "cost": "0",
+                    "notes": "已抵達曼谷",
+                    "mapURL": "https://maps.app.goo.gl/XEcdjEe9dxrpWvsR6"
+                },
+                {
+                    "time": "19:00-21:30",
+                    "location": "Save One Go Market 逛街用餐",
+                    "mapURL": "https://maps.app.goo.gl/8oMdiRPceXYnsXNq8",
+                    "transport": "Bolt",
+                    "cost": "250",
+                    "notes": "建議費用：160-250 泰銖 (車費)；免費廁所)；ต้องจ่ายค่าทางด่วนไหม?（我需要繳過路費嗎？）"
+                },
+                {
+                    "time": "21:30",
+                    "location": "BTS Yaek Kor Por Aor（N23） → Ratchathewi（N1）",
+                    "mapURL": "",
+                    "transport": "BTS",
+                    "cost": "58",
+                    "notes": "；飯店在 Ratchathewi 站附近；1號出口；"
+                },
+                {
+                    "time": "22:30",
+                    "location": "抵達 飯店 BaanSu Hostel",
+                    "mapURL": "https://maps.app.goo.gl/4RVqrgHZEdgMvz9M9",
+                    "transport": "步行/無",
+                    "cost": "0",
+                    "notes": "บ้านซูโฮสเทล；\nถนนนี้ไปถึงที่นั่นไหม? “Does this road come here?”（這條路會到這裡嗎？）"
+                }
+            ]
+        },
+        {
+            "day": "Day 2",
+            "date": "11/28 (五)",
+            "fullDate": "2025-11-28",
+            "theme": "購物探索",
+            "events": [
+                {
+                    "time": "08:00-08:40",
+                    "location": "ปุ้ย อาหารตามสั่ง 早餐",
+                    "mapURL": "https://maps.app.goo.gl/KQM1mnCba3ha8DuW6",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": ""
+                },
+                {
+                    "time": "08:50",
+                    "location": "Saphan Hua Chang碼頭 → Pratunam 碼頭",
+                    "transport": "船",
+                    "cost": "11",
+                    "notes": "船票 11 泰銖；ท่าเรือประตูน้ำ",
+                    "mapURL": "https://maps.app.goo.gl/cFProxMxw1SshFdL8"
+                },
+                {
+                    "time": "09:00-09:30",
+                    "location": "Grand Superrich換匯",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往",
+                    "mapURL": "https://maps.app.goo.gl/4xpb8jh8b5wAn8uf8"
+                },
+                {
+                    "time": "09:30-10:30",
+                    "location": "Big C",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "",
+                    "mapURL": "https://maps.app.goo.gl/CbMoUvckVezo2Day9", 
+                    "noteURL": "https://www.youtube.com/watch?v=ZRgRyB1Gdqk"
+                },
+                {
+                    "time": "10:30-11:40",
+                    "location": "象神、Central World",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "Central World 10點才開",
+                    "mapURL": "https://maps.app.goo.gl/NrcaKHXyhsXWvET4A"
+                },
+                {
+                    "time": "11:40-12:00",
+                    "location": "四面佛",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往",
+                    "mapURL": "https://maps.app.goo.gl/BxTUyirb7iHzWAxCA"
+                },
+                {
+                    "time": "12:15-13:45",
+                    "location": "Siam Center、暹羅廣場、20泰銖生醃海鮮",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "午餐/逛街",
+                    "mapURL": "https://maps.app.goo.gl/KcudWuaC1ggNgHhf8"
+                },
+                {
+                    "time": "13:50",
+                    "location": "BTS Siam（CEN）→ Asok（E4）",
+                    "transport": "BTS",
+                    "cost": "32",
+                    "notes": "4號出口",
+                    "mapURL": ""
+                },
+                {
+                    "time": "14:10-16:20",
+                    "location": "Terminal 21 用餐&逛街",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往",
+                    "mapURL": "https://maps.app.goo.gl/SksHVhrP9dQqB6sv5"
+                },
+                {
+                    "time": "16:00",
+                    "location": "MRT Sukhumvit（BL22）→Hua Lamphong（BL28）",
+                    "transport": "MRT",
+                    "cost": "27",
+                    "notes": "3號出口",
+                    "mapURL": ""
+                },
+                {
+                    "time": "17:05-18:30",
+                    "location": "Aey Seafood 晚餐",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往",
+                    "mapURL": "https://maps.app.goo.gl/TTe8VaJ6NU5vztoQ6"
+                },
+                {
+                    "time": "18:30-20:30",
+                    "location": "朱拉隆功夜市",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往",
+                    "mapURL": ""
+                },
+                {
+                    "time": "20:40",
+                    "location": "抵達 飯店",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行回飯店",
+                    "mapURL": "https://maps.app.goo.gl/4RVqrgHZEdgMvz9M9"
+                }
+            ]
+        },
+        {
+            "day": "Day 3",
+            "date": "11/29 (六)",
+            "fullDate": "2025-11-29",
+            "theme": "市集日",
+            "events": [
+                {
+                    "time": "08:45",
+                    "location": "Saphan Hua Chang碼頭 → Panfa Leelard 碼頭",
+                    "mapURL": "https://maps.app.goo.gl/9HTvdvC4obKFKnts6",
+                    "transport": "船",
+                    "cost": "12",
+                    "notes": "ท่าเรือผ่านฟ้าลีลาศ"
+                },
+                {
+                    "time": "09:00-09:50",
+                    "location": "王孫寺",
+                    "mapURL": "https://maps.app.goo.gl/2qx4Nzz8Eo6TYd3A7",
+                    "transport": "步行",
+                    "cost": "20",
+                    "notes": "門票 20 泰銖；收費廁所"
+                },
+                {
+                    "time": "10:05-11:50",
+                    "location": "南龍傳統市場 用餐",
+                    "mapURL": "https://maps.app.goo.gl/6QVgSKC8jN6qraAv8",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "市場內有廁所"
+                },
+                {
+                    "time": "12:00-13:00",
+                    "location": "Nangloeng Market → Wat Phai Tan (往恰圖恰方向)",
+                    "mapURL": "",
+                    "transport": "Bolt/公車",
+                    "cost": "160",
+                    "notes": "車費預計 130-160 泰銖"
+                },
+                {
+                    "time": "13:10-16:30",
+                    "location": "Chatuchak 週末市集",
+                    "mapURL": "https://maps.app.goo.gl/zx5PYNH7xp8kNzfLA",
+                    "transport": "無",
+                    "cost": "0",
+                    "notes": "百貨公司內有廁所",
+                    "noteURL": "https://onamap.me/maps/ChatuchakMarket/?fbclid=IwY2xjawN4UVhleHRuA2FlbQIxMABicmlkETE0RmFlbDFhUEJWSk1GTXdMc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHnwpFKQ9FKLWt28yRZ6uH7HEmRozTBj_jtU6YQCWqRkPutqvfiVgzoNeB3_3_aem_0bYM8iHSQnQyuxD85xECJA"
+                },
+                {
+                    "time": "16:40",
+                    "location": "BTS Mo Chit（N8）→ Ha Yaek Lat Phrao（N9）",
+                    "mapURL": "",
+                    "transport": "BTS",
+                    "cost": "15",
+                    "notes": "4號出口"
+                },
+                {
+                    "time": "17:00-19:00",
+                    "location": "Liabduan Danneramit 夜市",
+                    "mapURL": "https://maps.app.goo.gl/Q5YWVxmA8V8xNQky5",
+                    "transport": "BTS",
+                    "cost": "5",
+                    "notes": "夜市廁所 5 泰銖"
+                },
+                {
+                    "time": "19:05-20:00",
+                    "location": "Lotus’s Lat Phrao 超市",
+                    "mapURL": "https://maps.app.goo.gl/9i6xLL4ZEvHuZX6d7",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "免費廁所"
+                },
+                {
+                    "time": "20:05",
+                    "location": "BTS Ha Yaek Lat Phrao（N9）→ Ratchathewi（N1）",
+                    "mapURL": "",
+                    "transport": "BTS",
+                    "cost": "58",
+                    "notes": "1號出口回飯店"
+                }
+            ]
+        },
+        {
+            "day": "Day 4",
+            "date": "11/30 (日)",
+            "fullDate": "2025-11-30",
+            "theme": "寺廟與河畔",
+            "events": [
+                {
+                    "time": "08:30-10:30",
+                    "location": "大皇宮",
+                    "mapURL": "https://maps.app.goo.gl/A7gGMbr39LykyHWDA",
+                    "transport": "Bolt",
+                    "cost": "500",
+                    "notes": "車費預計 100-150 泰銖；門票 500 泰銖；免費廁所"
+                },
+                {
+                    "time": "10:50-11:50",
+                    "location": "ครัวพี่น้อง อาหารตามสั่ง 午餐",
+                    "mapURL": "https://maps.app.goo.gl/2benCaRwUvNQp7bN7",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往"
+                },
+                {
+                    "time": "11:55-13:00",
+                    "location": "拉查波比托寺",
+                    "mapURL": "https://maps.app.goo.gl/eJLNe9hqM78wiMzW7",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "免費廁所 (注意：廁所沒衛生紙)"
+                },
+                {
+                    "time": "13:15-14:55",
+                    "location": "臥佛寺",
+                    "mapURL": "https://maps.app.goo.gl/PLBcvUe9xZ4awXCX6",
+                    "transport": "步行",
+                    "cost": "300",
+                    "notes": "門票 300 泰銖；免費廁所"
+                },
+                {
+                    "time": "15:05-15:20",
+                    "location": "Tha Tien 碼頭（N8）",
+                    "mapURL": "https://maps.app.goo.gl/4RScZ4wrYMBhEjsL6",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": ""
+                },
+                {
+                    "time": "15:05-15:20",
+                    "location": "Tha Tien 碼頭（N8）→ 鄭王廟",
+                    "mapURL": "https://maps.app.goo.gl/H1A4nHjXxrt4GdP78",
+                    "transport": "渡輪",
+                    "cost": "5",
+                    "notes": "每10分鐘一班船"
+                },
+                {
+                    "time": "15:30-16:20",
+                    "location": "ใสสะอาด ก๋วยเตี๋ยวลูกชิ้นน้ำใส สาขาอรุณอมรินทร์ 用餐",
+                    "mapURL": "https://maps.app.goo.gl/UqDggDHZAXKzNDut6",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "步行前往"
+                },
+                {
+                    "time": "16:30-18:00",
+                    "location": "鄭王廟",
+                    "mapURL": "https://maps.app.goo.gl/5spoMeseArNcMB1v7",
+                    "transport": "步行",
+                    "cost": "200",
+                    "notes": "門票 200 泰銖；免費廁所"
+                },
+                {
+                    "time": "18:00-18:10",
+                    "location": "藍船 → ICONSIAM",
+                    "mapURL": "https://maps.app.goo.gl/gfMXf4mbkZkLoiEj8",
+                    "transport": "船",
+                    "cost": "40",
+                    "notes": "船票 40 泰銖；或搭 18:40 班次；記得坐左邊"
+                },
+                {
+                    "time": "18:10-20:30",
+                    "location": "ICONSIAM 逛街",
+                    "mapURL": "",
+                    "transport": "無",
+                    "cost": "0",
+                    "notes": "抵達後先去換匯；有免費廁所"
+                },
+                {
+                    "time": "20:30-20:40",
+                    "location": "接駁船 → Sathorn碼頭",
+                    "mapURL": "https://maps.app.goo.gl/YZsLBZR213QeEjSh9",
+                    "transport": "船",
+                    "cost": "0",
+                    "notes": "GF 2號門上船"
+                },
+                {
+                    "time": "20:40",
+                    "location": "BTS 回飯店",
+                    "mapURL": "https://maps.app.goo.gl/fwtscEvcs2djsxit8",
+                    "transport": "BTS",
+                    "cost": "51",
+                    "notes": "BTS Saphan Taksin(S6) → National Stadium(W1)；走水溝巷子回飯店"
+                }
+            ]
+        },
+        {
+            "day": "Day 5",
+            "date": "12/1 (一)",
+            "fullDate": "2025-12-01",
+            "theme": "大城一日遊",
+            "events": [
+                {
+                    "time": "09:00",
+                    "location": "飯店集合",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "包車一日遊開始",
+                    "mapURL": ""
+                },
+                {
+                    "time": "10:45-11:40",
+                    "location": "Prasat Nakhon Luang",
+                    "mapURL": "https://maps.app.goo.gl/T4XBoZhYw8G6umrt9",
+                    "transport": "包車",
+                    "cost": "20",
+                    "notes": "門票 20 泰銖"
+                },
+                {
+                    "time": "12:00-13:00",
+                    "location": "Kung Phuean Praew 午餐",
+                    "mapURL": "https://maps.app.goo.gl/Steo4JcNdiS2rkzp9",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "午餐地點"
+                },
+                {
+                    "time": "13:15-14:10",
+                    "location": "崖差蒙空寺",
+                    "mapURL": "https://maps.app.goo.gl/wBedJu1YvJfGAw7M6",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": ""
+                },
+                {
+                    "time": "14:20-14:40",
+                    "location": "三寶公寺",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "",
+                    "mapURL": "https://maps.app.goo.gl/H1K7X9H25F57gQ7M9"
+                },
+                {
+                    "time": "14:55-15:40",
+                    "location": "柴瓦塔那蘭寺",
+                    "mapURL": "https://maps.app.goo.gl/vYWqGfLC6jRALCzEA",
+                    "transport": "包車",
+                    "cost": "80",
+                    "notes": "門票 80 泰銖"
+                },
+                {
+                    "time": "15:50-16:05",
+                    "location": "Roti Sai Mai Abeedeen-Pranom Sangaroon 糖絲卷餅",
+                    "mapURL": "https://maps.app.goo.gl/Q8vzJbu4oypvtsNt9",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "大城特色甜點"
+                },
+                {
+                    "time": "16:10-16:50",
+                    "location": "Pa Lek Boat Noodle 晚餐",
+                    "mapURL": "https://maps.app.goo.gl/gPtuwsvJEyiR9mo2A",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "晚餐地點"
+                },
+                {
+                    "time": "16:55-18:00",
+                    "location": "瑪哈泰寺",
+                    "mapURL": "https://maps.app.goo.gl/88s6ZQkrSbwUwry58",
+                    "transport": "步行",
+                    "cost": "80",
+                    "notes": "門票 80 泰銖；注意：17點前要入場"
+                },
+                {
+                    "time": "18:05-19:10",
+                    "location": "Ong Gaan Tolasap 夜市",
+                    "mapURL": "https://maps.app.goo.gl/zAtkmgiKMjabvyxu9",
+                    "transport": "步行",
+                    "cost": "0",
+                    "notes": "大城夜市"
+                },
+                {
+                    "time": "20:30",
+                    "location": "DMK 機場",
+                    "mapURL": "https://maps.app.goo.gl/XEcdjEe9dxrpWvsR6",
+                    "transport": "包車",
+                    "cost": "0",
+                    "notes": "搭乘包車前往機場"
+                },
+                {
+                    "time": "2:45-7:15",
+                    "location": "Thai Lion Air SL390",
+                    "mapURL": "",
+                    "transport": "飛機",
+                    "cost": "0",
+                    "notes": ""
+                },
+            ]
+        }
+    ]
+};
